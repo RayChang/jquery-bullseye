494eef574c68490ed66821158537a92b8a6d7446
Ray Chang <mofies@gmail.com>
2014-10-06 14:47:13 +0800

    更新程式碼並加入新功能
    當視窗縮放時圖片會重新調整比例

--- src/jquery-bullseye.js
+++ src/jquery-bullseye.js
@@ -1,108 +1,102 @@
 !function ($) {
   "use strict";
+  var defaults = {
+    fadeEffect: true,
+    effectTime: 0.5
+  };
+  var $window = $(window);
+  var setupImageContainer = function($element, settings) {
+    var _imageSrc,
+        image = document.createElement('img');
+    if($element.data('bullseyeImage')) {
+      $element.html('<img src="' + $element.data('bullseyeImage') + '">');
+      _imageSrc = $element.data('bullseyeImage');
+    } else {
+      _imageSrc = $element.find('img').first().attr('src');
+      $element.data('bullseyeImage', _imageSrc);
+    }
+    settings.fadeEffect ? $element.find('img').first().css({'opacity': 0}) : null;
 
-  $.fn.bullseye = function(options) {
-    return this.each(function() {
-      var $element,
-          $elementImage,
-          $image,
-          _initial,
-          _fadeCss,
-          _elementInlineStyle,
-          _elementWidth,
-          _elementHeight,
-          _imageInlineStyle,
-          _imageWidth,
-          _imageHeight,
-          _scale,
-          imageLoadComplete;
-
-      _initial = {
-        windowResponse: false,
-        fadeEffect: false,
-        effectTime: .5
-      };
-
-      _initial = $.extend(true, _initial, options);
-
-      _fadeCss = {
-        start: {
-          'opacity': 1,
-          '-webkit-transition': 'opacity ' + _initial.effectTime + 's ease-in-out',
-          '-moz-transition': 'opacity ' + _initial.effectTime + 's ease-in-out',
-          '-o-transition': 'opacity ' + _initial.effectTime + 's ease-in-out',
-          'transition': 'opacity ' + _initial.effectTime + 's ease-in-out'
+    image.src = _imageSrc;
+    image.onload = function() {
+      adjustImage($element, settings);
+    };
+  };
+  var adjustImage = function($element, settings) {
+    var $image = $element.find('img').first(),
+        _scale,
+        _imageW,
+        _imageH,
+        _elementW = $element.innerWidth(),
+        _elementH = $element.innerHeight(),
+        _elementStyle = {
+          'position': 'relative',
+          'overflow': 'hidden'
         },
-        end: {
-          'opacity': '',
-          '-webkit-transition': '',
-          '-moz-transition': '',
-          '-o-transition': '',
-          'transition': ''
-        }
-      }
-
-      $element = $(this);
-      _elementWidth = $element.innerWidth();
-      _elementHeight = $element.innerHeight();
-
-      $image = document.createElement('img');
-
-      if($element.data('bullseyeImage')) {
-        $element.append('<img src="' + $element.data('bullseyeImage') + '">');
-        $image.src = $element.data('bullseyeImage');
-      } else {
-        $image.src = $element.children('img').eq(0).attr('src');
-      }
-
-      $elementImage = $element.children('img').eq(0);
-      _initial.fadeEffect ? $elementImage.css({'opacity': 0}) : null;
-
-      _elementInlineStyle = {
-        'position': 'relative',
-        'overflow': 'hidden'
-      };
-      _imageInlineStyle = {
-        'position': 'absolute',
-        'top': 0,
-        'right': 0,
-        'bottom': 0,
-        'left': 0,
-        'margin': 'auto',
-        'width': '100%',
-        'height': 'auto'
-      }
-
-      $element.css(_elementInlineStyle);
-      $elementImage.css(_imageInlineStyle)
-      imageLoadComplete = function() {
-        _imageWidth = $elementImage.innerWidth();
-        _imageHeight = $elementImage.innerHeight()*(_elementWidth/$elementImage.innerWidth());
-
-        if(_imageHeight < _elementHeight) {
-          _scale = _elementHeight / _imageHeight
-          $elementImage.css({
-            '-webkit-transform': 'scale(' + _scale + ')',
-            '-moz-transform': 'scale(' + _scale + ')',
-            '-o-transform': 'scale(' + _scale + ')',
-            'transform': 'scale(' + _scale + ')'
-          });
-        } else {
-          $elementImage.css({
-            '-webkit-transform': '',
-            '-moz-transform': '',
-            '-o-transform': '',
-            'transform': ''
-          });
-        }
-
-        if(_initial.fadeEffect) {
-          $elementImage.css(_fadeCss.start).on('transitionend', function() {
-            $(this).css(_fadeCss.end)
-          });
+        _imageStyle = {
+          'position': 'absolute',
+          'top': 0,
+          'right': 0,
+          'bottom': 0,
+          'left': 0,
+          'margin': 'auto',
+          'width': '100%',
+          'height': 'auto'
+        },
+        _fadeCss = {
+          start: {
+            'opacity': 1,
+            '-webkit-transition': 'opacity ' + settings.effectTime + 's ease-in-out',
+            '-moz-transition': 'opacity ' + settings.effectTime + 's ease-in-out',
+            '-o-transition': 'opacity ' + settings.effectTime + 's ease-in-out',
+            'transition': 'opacity ' + settings.effectTime + 's ease-in-out'
+          },
+          end: {
+            'opacity': '',
+            '-webkit-transition': '',
+            '-moz-transition': '',
+            '-o-transition': '',
+            'transition': ''
+          }
         };
-      }
-      $image.onload = imageLoadComplete;
+
+    $element.css(_elementStyle);
+    $image.css(_imageStyle);
+    _imageW = $image.width();
+    _imageH = $image.height();
+    if(_imageH < _elementH) {
+      _scale = _elementH / _imageH
+      $image.css({
+        '-webkit-transform': 'scale(' + _scale + ')',
+        '-moz-transform': 'scale(' + _scale + ')',
+        '-o-transform': 'scale(' + _scale + ')',
+        'transform': 'scale(' + _scale + ')'
+      });
+    } else {
+      $image.css({
+        '-webkit-transform': '',
+        '-moz-transform': '',
+        '-o-transform': '',
+        'transform': ''
+      });
+    }
+    if(settings.fadeEffect) {
+      $image.css(_fadeCss.start).on('transitionend', function() {
+        $(this).css(_fadeCss.end);
+      });
+    };
+  };
+  var bullseye = function($element, settings) {
+    setupImageContainer($element, settings);
+    $window.on('resize', function(event) {
+      adjustImage($element, settings);
+    });
+  };
+  $.fn.bullseye = function(options) {
+    var settings = $.extend({}, defaults, options);
+    return this.each(function() {
+      var $element = $(this);
+      bullseye($element, settings);
     });
   };
 }(window.jQuery);
